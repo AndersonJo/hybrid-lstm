6cb2597b88a8af480626d5a84b649a7cc74640e3
Anderson <a141890@gmail.com>
2017-08-25 01:24:40 +0900

    haha

--- dataset.py
+++ dataset.py
@@ -4,15 +4,25 @@ import re
 import pandas as pd
 
 
-def load_wiki_traffic_dataset(file_path: str):
-    if not os.path.exists(file_path):
-        raise FileNotFoundError('file not found')
-    train = pd.read_csv('/dataset/web-traffic-forecast/train_1.csv').fillna(0.)
-    train.columns = list(train.columns[:1]) + list(range(1, len(train.columns[1:]) + 1))
+def load_wiki_traffic_dataset(train_path: str, test_path: str):
+    if not os.path.exists(train_path):
+        raise FileNotFoundError('Train dataset not found')
+    if not os.path.exists(test_path):
+        raise FileNotFoundError('Test dataset not found')
 
-    r = re.compile('(?P<page>.+)_(?P<country>[a-z]+)\.'
-                   '(?P<project>[a-z]+)\.org_'
-                   '(?P<access>\w*-?\w*)_(?P<agent>\w+)')
+    train_dataset = pd.read_csv(train_path).fillna(0.)
+    test_dataset = pd.read_csv(test_path)
 
-    train = pd.concat((train['Page'].str.extract(r, expand=False), train.ix[:, train.columns != 'Page']), axis=1)
-    return train
+    # Preprocessing Train Dataset
+    # train_dataset.columns = list(train_dataset.columns[:1]) + list(range(1, len(train_dataset.columns[1:]) + 1))
+
+    train_regex = re.compile('(?P<page>.+)_(?P<country>[a-z]+)\.'
+                             '(?P<project>[a-z]+)\.org_'
+                             '(?P<access>\w*-?\w*)_(?P<agent>\w+)')
+    train_dataset = pd.concat((train_dataset['Page'].str.extract(train_regex, expand=True),
+                               train_dataset.ix[:, train_dataset.columns != 'Page']), axis=1)
+
+    # Preprocessing Test Dataset
+    test_regex = re.compile('(?P<name>.+)_(?P<date>\d{4}-\d{2}-\d{2})')
+    test_dataset = pd.concat([test_dataset['Page'].str.extract(test_regex, expand=True), test_dataset['Id']], axis=1)
+    return train_dataset, test_dataset
--- test.py
+++ test.py
@@ -2,7 +2,8 @@ from dataset import load_wiki_traffic_dataset
 
 
 def test_load_dataset():
-    file_path = '/dataset/web-traffic-forecast/train_1.csv'
-    train = load_wiki_traffic_dataset(file_path)
+    train_path = '/dataset/web-traffic-forecast/train_1.csv'
+    test_path = '/dataset/web-traffic-forecast/key_1.csv'
+    train = load_wiki_traffic_dataset(train_path, test_path)
 
     assert train[train.isnull().any(axis=1)].shape[0] == 0
